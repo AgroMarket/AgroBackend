516522528817484d7181ce68a1baebe351553eca
Alexey Tyugaev <alexx10@mail.ru>
2018-10-21 10:47:37 +0300

    Added skip mail notification in User model and added autocomplete for User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   rolify
+  before_create :skip_confirmation_notification!
+  before_update :skip_confirmation_notification!
+
   # Include default devise modules. Others available are:
   devise :database_authenticatable,
          :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Role.destroy_all
+
+def random_inn_kpp
+  inn = ""
+  9.times do
+    inn += rand(0..9).to_s
+  end
+  inn
+end
+
+users = 5.times.map do
+  {
+    email: FFaker::Internet.safe_email,
+    fio: FFaker::NameRU.name,
+    password: 'password',
+    inn: random_inn_kpp,
+    kpp: random_inn_kpp,
+    telephone: FFaker::PhoneNumber.short_phone_number,
+    region: FFaker::AddressRU.city,
+    another_information: FFaker::HipsterIpsum.paragraph
+  }
+end
+
+User.create! users
