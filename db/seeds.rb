65b9c130147f5b2fab1022713331544dd3a9446f
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-25 15:26:38 +0700

    add categories to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,3 +52,97 @@ end
 
 u = User.first
 u.roles = Role.all
+
+
+# Categories
+Category.destroy_all
+category_names = [
+    {name: 'Мёд', children: [
+        {name: 'Мёд'},
+        {name: 'Мёд в сотах'},
+        {name: 'Продукты пчеловодства'}
+    ]},
+    {name: 'Ягоды', children: [
+        {name: 'Свежие ягоды'},
+        {name: 'Замороженные ягоды'},
+        {name: 'Сущеные ягоды'}
+    ]},
+    {name: 'Овощи фрукты', children: [
+        {name: 'Зелень'},
+        {name: 'Овощи'},
+        {name: 'Фрукты'}
+    ]},
+    {name: 'Орехи', children: [
+        {name: 'Орехи'},
+        {name: 'Семечки'},
+        {name: 'Орехи, семечки очищенные'}
+    ]},
+    {name: 'Молочные продукты', children: [
+        {name: 'Молоко, сливки'},
+        {name: 'Кефир, ряженка'},
+        {name: 'Сметана'},
+        {name: 'Сыр'},
+        {name: 'Творог'}
+    ]},
+    {name: 'Крупы, Бобовые', children: [
+        {name: 'Гречка'},
+        {name: 'Пшеница'},
+        {name: 'Пшено'},
+        {name: 'Горох'},
+        {name: 'Фасоль'},
+        {name: 'Соя'},
+        {name: 'Чечевица'}
+    ]},
+    {name: 'Готовые продукты, заготовки', children: [
+        {name: 'Хлебобулочные изделия'},
+        {name: 'Консервированные продукты'},
+        {name: 'Мясные деликатесы'},
+        {name: 'Пельмени, вареники'},
+        {name: 'Соусы, специи'},
+        {name: 'Кондитерские изделия'}
+    ]},
+    {name: 'Птица, Яйцо', children: [
+        {name: 'Курица'},
+        {name: 'Индейка'},
+        {name: 'Гусь'},
+        {name: 'Утка'},
+        {name: 'Яйцо'}
+    ]},
+    {name: 'Рыба, Морепродукты', children: [
+        {name: 'Замороженная рыба'},
+        {name: 'Копченная, соленая, вяленная рыба'},
+        {name: 'Свежая рыба'},
+        {name: 'Икра'}
+    ]},
+    {name: 'Грибы', children: [
+        {name: 'Свежие грибы'},
+        {name: 'Замороженные грибы'},
+        {name: 'Соленые грибы'},
+        {name: 'Сушенные грибы'}
+    ]},
+    {name: 'Напитки', children: [
+        {name: 'Соки'},
+        {name: 'Морс'},
+        {name: 'Квас'},
+        {name: 'Сиропы'},
+        {name: 'Чай'}
+    ]},
+    {name: 'Масла, Жиры', children: [
+        {name: 'Масло сливочное'},
+        {name: 'Масло растительное'},
+        {name: 'Жир животный'}
+    ]},
+    {name: 'Мясо', children: [
+        {name: 'Говядина'},
+        {name: 'Свинина'},
+        {name: 'Баранина'},
+        {name: 'Крольчатина'},
+        {name: 'Субпродукты'}
+    ]},
+]
+category_names.each_with_index do |category_name, idx|
+  category = Category.create! name: category_name[:name], parent_id: 0, rank: idx + 1
+  category_name[:children].each_with_index do |sub_name, sub_idx|
+    category.subcategories << Category.create!(name: sub_name[:name], parent_id: category.id, rank: sub_idx)
+  end
+end
