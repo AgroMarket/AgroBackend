0382ac1c04c0163024b8f28e47144565ecbda4c0
Alexey Tyugaev <alexx10@mail.ru>
2018-11-02 06:51:44 +0300

    default role test rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     expect(FactoryBot.create(:user)).to be_valid
   end
+
+  context 'default role' do
+    let(:user) { FactoryBot.create(:user)}
+    it {expect(user.roles[0].name).to eq('customer')}
+  end
+
 end
