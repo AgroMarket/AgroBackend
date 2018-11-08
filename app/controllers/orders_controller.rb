e170a6b49c3ddb051661e0bdb3a56a1230914f43
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-08 16:35:42 +0700

    generate scaffold Order --skip

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,53 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :update, :destroy]
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      render :show, status: :created, location: @order
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /orders/1
+  # PATCH/PUT /orders/1.json
+  def update
+    if @order.update(order_params)
+      render :show, status: :ok, location: @order
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /orders/1
+  # DELETE /orders/1.json
+  def destroy
+    @order.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.fetch(:order, {})
+    end
+end
