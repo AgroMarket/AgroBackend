f344e0c25f5d50c74bb091e9426f2f46270a9810
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-11 03:23:36 +0700

    edit Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@ class Cart < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
 
-  has_many :items, dependent: :destroy
+  has_many :items
 
   # добавляет один продукт
   def add_product product
