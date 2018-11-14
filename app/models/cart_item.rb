89cfce4751b5d2a6e034cc6054306f9f6c5e1014
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-14 19:42:50 +0700

    auto calculate sum, total

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,26 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
   belongs_to :producer, optional: true
+
+  before_create   :prepare_cart_item
+  before_save     :calculate_sum
+  after_create    :calculate_cart_total
+  after_save      :calculate_cart_total
+  after_destroy   :calculate_cart_total
+
+  private
+
+  def prepare_cart_item
+    self.producer = product.producer
+    calculate_sum
+  end
+
+  def calculate_sum
+    self.sum = product.price * quantity
+  end
+
+  def calculate_cart_total
+    cart.total = cart.cart_items.map(&:sum).inject { |total, sum| total + sum }
+    cart.save
+  end
 end
