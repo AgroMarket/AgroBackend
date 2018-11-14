00fde831a4768ff5f077463c41af17256d14b5df
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-14 19:52:50 +0700

    create orders_controller in consumer namespace

---
+++ app/controllers/consumer/orders_controller.rb
@@ -0,0 +1,60 @@
+class Consumer::OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :update, :destroy]
+  include Exceptable
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    build do
+      message 'Покупки пользователя'
+      orders Order.where(consumer_id: current_user.id)
+      view 'consumer/orders/index'
+    end
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    if Order.create_orders_from_cart(params[:cart_id])
+      build do
+        message 'Создание заказов'
+        view 'orders/create'
+      end
+      # render :show, status: :created, location: @order
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /orders/1
+  # PATCH/PUT /orders/1.json
+  def update
+    if @order.update(order_params)
+      render :show, status: :ok, location: @order
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /orders/1
+  # DELETE /orders/1.json
+  def destroy
+    @order.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.fetch(:order, {})
+    end
+end
