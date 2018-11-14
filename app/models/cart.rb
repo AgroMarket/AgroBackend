8bf10097d4746a3a6e5ad8f077913c553f31d028
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-14 19:41:20 +0700

    refactor Cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :consumer, optional: true
   has_many :cart_items, dependent: :destroy
 
-  def calculate_order_total
-    self.total = cart_items.map(&:sum).inject { |total, sum| total + sum }
-  end
+  # def calculate_cart_total
+  #   self.total = cart_items.map(&:sum).inject { |total, sum| total + sum }
+  #   save
+  # end
 end
