d72b98ed2e913aaec7d45721c2cba39e81a77d08
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-02 23:27:29 +0700

    some refactor

--- app/controllers/consumer/transactions_controller.rb
+++ app/controllers/consumer/transactions_controller.rb
@@ -1,60 +1,62 @@
 class Consumer::TransactionsController < ApplicationController
-   # before_action :set_tranzaction, only: [:show, :update, :destroy]
-    before_action :authenticate_user
-    include Exceptable
-  
-    # GET /tranzactions
-    # GET /tranzactions.json
-    def index
-        @transactions = Transaction.where(from: current_user.id) 
-        build do
-            message "Транзакции пользователя"   
-            view 'consumer/transactions/transaction'
-        end
-    end  
+  # before_action :set_tranzaction, only: [:show, :update, :destroy]
+  before_action :authenticate_user
+  include Exceptable
 
-    def show
+  # GET /tranzactions
+  # GET /tranzactions.json
+  def index
+    @transactions = Transaction.where(from: current_user.id)
+    build do
+      message "Транзакции пользователя"
+      view 'consumer/transactions/transaction'
     end
-
-    def create
-        @transaction = Transaction.new(transaction_params)
-        if @transaction.status == "Пополнение"
-            @transaction.from = current_user
-            @transaction.to = current_user
-            @transaction.ask = nil
-            @transaction.order = nil
-            current_user.amount += @transaction.amount
-            current_user.save
-            puts current_user.amount
-            if @transaction.save!
-                render :show, status: :created, json: @transaction
-            else
-                render json: @transaction.errors, status: :unprocessable_entity
-            end
-        elsif @transaction.status == "Вывод"
-            @transaction.from = current_user
-            @transaction.to = current_user
-            @transaction.ask = nil
-            @transaction.order = nil
-            if current_user.amount >= @transaction.amount 
-                current_user.amount -= @transaction.amount
-                current_user.save
-                @transaction.save!
-                render :show, status: :created, json: @transaction
-            else
-                build do
-                    message "На счёте недостаточно средств"
-                    view 'consumer/transactions/response'
-                end
-            end
-        end
-    end
-
-    private
-
-    def transaction_params
-        params.require(:transaction).permit(:amount, :status)
-    end
-    
   end
+
+  def show
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.status == "Пополнение"
+      @transaction.from = current_user
+      @transaction.to = current_user
+      @transaction.ask = nil
+      @transaction.order = nil
+      current_user.amount += @transaction.amount
+      current_user.save
+      puts current_user.amount
+      # TODO оформить json
+      if @transaction.save!
+        render :show, status: :created, json: @transaction
+      else
+        render json: @transaction.errors, status: :unprocessable_entity
+      end
+    elsif @transaction.status == "Вывод"
+      @transaction.from = current_user
+      @transaction.to = current_user
+      @transaction.ask = nil
+      @transaction.order = nil
+      if current_user.amount >= @transaction.amount
+        current_user.amount -= @transaction.amount
+        current_user.save
+        @transaction.save!
+        # TODO оформить json
+        render :show, status: :created, json: @transaction
+      else
+        build do
+          message "На счёте недостаточно средств"
+          view 'consumer/transactions/response'
+        end
+      end
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :status)
+  end
+
+end
   
