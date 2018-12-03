244adaea6f87a99ba36b191b0da7b465c9da1c4b
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-03 13:11:20 +0700

    refactor Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
-  belongs_to :from, :class_name => 'User'
-  belongs_to :to, :class_name => 'User'
+  belongs_to :from, class_name: 'User'
+  belongs_to :to, class_name: 'User'
   belongs_to :ask, optional: true
   belongs_to :order, optional: true
 
