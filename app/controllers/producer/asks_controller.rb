740563986e1ed7896d46a79edff2c61dd674f386
Alexey Tyugaev <alexx10@mail.ru>
2018-12-03 18:54:53 +0300

    add asks to producer

---
+++ app/controllers/producer/asks_controller.rb
@@ -0,0 +1,92 @@
+class Consumer::AsksController < ApplicationController
+  before_action :set_ask, only: [:show, :update, :destroy]
+  include Exceptable
+
+  # GET /asks
+  # GET /asks.json
+  def index
+    build do      
+      @asks = Ask.where(consumer: current_user).includes(:orders).order('created_at DESC')
+      message 'Список заказов покупателя'
+      view 'consumer/asks/index'
+    end
+  end
+
+  # GET /asks/1
+  # GET /asks/1.json
+  def show
+    build do
+      message 'Заказ покупателя детально'
+      view 'consumer/asks/show'
+    end
+  end
+
+  # POST /asks
+  # POST /asks.json
+  def create
+    @ask = Ask.new(ask_params)
+    @ask.consumer = current_user
+    @ask.amount += 500
+    if current_user.amount >= @ask.amount
+      if @ask.save
+        build do
+          message 'Оформление заказа'
+          #create_transaction(current_user, current_user, @ask.amount, @ask, order=nil, "Пополнение")
+          create_transaction(current_user, fermastore, @ask.amount, @ask, order=nil, 1)
+          status :created
+          view 'consumer/asks/show'
+        end
+      else
+        build do
+          message 'Оформление заказа'
+          error @ask.errors
+          status :unprocessable_entity
+          view 'consumer/asks/show'
+        end
+      end
+    else
+      build do
+        message 'На счёте недостаточно средств'
+        view 'consumer/transactions/response'
+      end
+    end
+    
+  end
+
+  # PATCH/PUT /asks/1
+  # PATCH/PUT /asks/1.json
+  def update
+    if params[:ask][:status] == 2 && @ask.status != 2
+      @ask.orders.each do |order|
+        create_transaction(fermastore, order.producer, (order.total*0.9).to_i, @ask, order, 2)
+      end
+      create_transaction(fermastore, money_user, (@ask.amount*0.1).to_i, @ask, nil, 2)
+      create_transaction(fermastore, tk_user, 500, @ask, nil, 2)
+      if @ask.update(ask_params)
+        build do 
+          message 'Статус заказа изменен'
+          view 'consumer/asks/show'
+        end
+      else
+        render json: @ask.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  # DELETE /asks/1
+  # DELETE /asks/1.json
+  def destroy
+    @ask.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ask
+      @ask = Ask.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def ask_params
+      params.require(:ask).permit(:amount, :status)
+    end
+end
---
+++ app/views/producer/asks/_index.json.jbuilder
@@ -0,0 +1,21 @@
+# json.array! @asks, partial: 'asks/ask', as: :ask
+
+json.asks do
+  json.array! @asks do |ask|
+    json.ask do
+      json.id ask.id
+      json.date ask.created_at
+      json.amount ask.amount
+      json.status ask.status
+      json.link consumer_ask_path(ask.id)
+      # json.orders do
+      #   json.array! ask.orders do |order|
+      #     json.order do
+      #       json.id order.id
+      #       json.order
+      #     end
+      #   end
+      # end
+    end
+  end
+end
