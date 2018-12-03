61775d920e8a839cfc246f37dce0d85a1f5492e6
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-03 13:13:23 +0700

    fix producer/producers_controller

--- app/controllers/producer/producers_controller.rb
+++ app/controllers/producer/producers_controller.rb
@@ -20,7 +20,12 @@ class Producer::ProducersController < ApplicationController
         view 'producer/producers/show'
       end
     else
-      render json: @producer.errors, status: :unprocessable_entity
+      build do
+        message 'Редактирование профиля производителя'
+        error @producer.errors
+        status :unprocessable_entity
+        view 'producer/producers/show'
+      end
     end
   end
 
@@ -32,6 +37,6 @@ class Producer::ProducersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def producer_params
-      params.require(:producer).permit(:email, :password, :name, :phone, :address, :producer_logo, :producer_brand, :producer_description, :producer_address, :producer_phone, :inn)
+      params.require(:producer).permit(:email, :password, :name, :phone, :address, :producer_logo, :producer_brand, :producer_description, :producer_address, :producer_phone, :producer_inn)
     end
 end
