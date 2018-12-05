731ece4afb1130973294ee0609a012d952c17240
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-06 00:31:25 +0700

    refactor asks for Member

--- app/models/ask.rb
+++ app/models/ask.rb
@@ -1,5 +1,5 @@
 class Ask < ApplicationRecord
-  belongs_to :consumer
+  belongs_to :consumer, class_name: 'Member', foreign_key: 'consumer_id'
   has_many :orders, dependent: :destroy
   has_many :tranzactions
   has_many :tasks
--- db/migrate/20181025191126_create_asks.rb
+++ db/migrate/20181025191126_create_asks.rb
@@ -1,7 +1,7 @@
 class CreateAsks < ActiveRecord::Migration[5.2]
   def change
     create_table :asks do |t|
-      t.references :consumer, index: true
+      t.integer :consumer_id, index: true
       t.integer :amount, null: false, default: 0
       t.integer :status, null: false, default: 0
 
