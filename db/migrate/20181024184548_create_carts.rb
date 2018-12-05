19a970cf3bf4801a06ab0024db575acb741bf2d9
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-05 17:15:59 +0700

    change cart structure

--- db/migrate/20181024184548_create_carts.rb
+++ db/migrate/20181024184548_create_carts.rb
@@ -2,7 +2,9 @@ class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
       t.references :consumer, index: true
-      t.integer :total
+      t.integer :sum, default: 0
+      t.integer :delivery_cost, default: 0
+      t.integer :total, default: 0
 
       t.timestamps
     end
--- db/migrate/20181201125523_add_column_delivery_cost_to_carts.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnDeliveryCostToCarts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :carts, :delivery_cost, :integer, default: 500
-  end
-end
