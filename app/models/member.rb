e624935d38a198bd78321cea1f628540f8c44392
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-06 00:29:11 +0700

    upgrade Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,13 @@
-class Member < ApplicationRecord
+class Member < User
+  has_many :products, foreign_key: 'producer_id'
+  has_one_attached :image
+  has_one_attached :logo
+
+  def self.consumers
+    Member.where(user_type: 'consumer')
+  end
+
+  def self.producers
+    Member.where(user_type: 'producer')
+  end
 end
