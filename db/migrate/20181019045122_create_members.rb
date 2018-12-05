1c87ff00ca6f01ddc5e7dbdd5d7a924926a2fc8b
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-05 21:56:40 +0700

    change member table

--- db/migrate/20181019045122_create_members.rb
+++ db/migrate/20181019045122_create_members.rb
@@ -1,19 +1,20 @@
 class CreateMembers < ActiveRecord::Migration[5.2]
   def change
     change_table :users do |t|
-      t.integer :amount
-      t.string :type
-      t.string :image
-      t.string :name
-      t.string :address
-      t.string :phone
-      t.text :description
-      t.string :producer_logo
-      t.string :producer_brand
-      t.string :producer_address
-      t.string :producer_phone
-      t.text :producer_description
-      t.string :producer_inn
+      t.string :type,               null: false, default: 'Member'
+      t.string :user_type,          null: false, default: 'consumer'
+      t.integer :amount,            null: false, default: 0
+      t.string :image,              null: false, default: ''
+      t.string :name,               null: false, default: ''
+      t.string :address,            null: false, default: ''
+      t.string :phone,              null: false, default: ''
+      t.text :description,          null: false, default: ''
+      t.string :producer_logo,      null: false, default: ''
+      t.string :producer_brand,     null: false, default: ''
+      t.string :producer_address,   null: false, default: ''
+      t.string :producer_phone,     null: false, default: ''
+      t.text :producer_description, null: false, default: ''
+      t.string :producer_inn,       null: false, default: ''
     end
   end
 end
