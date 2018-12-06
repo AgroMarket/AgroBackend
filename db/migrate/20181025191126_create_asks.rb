bdb11412ae95dc27186fca593d64b555ca665ebd
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-06 21:49:18 +0700

    change ask table

--- db/migrate/20181025191126_create_asks.rb
+++ db/migrate/20181025191126_create_asks.rb
@@ -2,7 +2,9 @@ class CreateAsks < ActiveRecord::Migration[5.2]
   def change
     create_table :asks do |t|
       t.integer :consumer_id, index: true
-      t.integer :amount, null: false, default: 0
+      t.integer :sum, default: 0
+      t.integer :delivery_cost, default: 0
+      t.integer :total, null: false, default: 0
       t.integer :status, null: false, default: 0
 
       t.timestamps
