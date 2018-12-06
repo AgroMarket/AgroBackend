a54939121b07798bde27d82edf7ffe45d24fc8e5
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-07 05:13:22 +0700

    refactor Member for orders

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,20 @@
 class Member < User
   has_many :products, foreign_key: 'producer_id'
-  has_many :sells, class_name: 'Order', foreign_key: 'producer_id'
-  has_many :consumers, through: :sells
+  has_many :orders, foreign_key: 'producer_id'
+  has_many :consumers, through: :orders
   has_many :asks, foreign_key: 'consumer_id'
   has_many :buys, class_name: 'Order', foreign_key: 'consumer_id'
   has_many :producers, through: :buys
   has_one_attached :image
   has_one_attached :logo
+
+  def buyers
+    consumers.distinct
+  end
+
+  def sellers
+    producers.distinct
+  end
 
   def self.consumers
     Member.where(user_type: 'consumer')
@@ -16,7 +24,4 @@ class Member < User
     Member.where(user_type: 'producer')
   end
 
-  def consumers
-
-  end
 end
