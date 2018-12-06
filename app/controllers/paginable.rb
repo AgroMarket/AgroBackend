079b4752d8d61f487e501c8d16373d85b316c5a5
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-07 00:23:40 +0700

    fix paginable

--- app/controllers/paginable.rb
+++ app/controllers/paginable.rb
@@ -1,15 +1,19 @@
 module Paginable
   def paginate(items_all)
+    # puts "items_all #{items_all.size}"
     per_page = 8
+    # puts "per_pate #{per_page}"
     result = items_all.page(params[:page]).per(per_page)
-    total_pages = (items_all.count / per_page).ceil
+    # puts "result #{result.size}"
+    total_pages = (items_all.count.to_f / per_page.to_f).ceil
+    # puts "total_pages #{total_pages}"
     current_page = params[:page] ? params[:page].to_i : 1
     url_params = @url_params.nil? ? nil : @url_params.map { |key, value| "#{key}=#{value}" }.to_a.join('&')
 
     @pagination = {
       "current_page": current_page,
       "first_page": 1,
-      "last_page": total_pages.zero? ? 1 : total_pages,
+      "last_page": total_pages,
       "prev_page_url": current_page <= 1 ? nil : "#{@path}?#{url_params}&page=#{current_page - 1}",
       "next_page_url": current_page >= total_pages ? nil : "#{@path}?#{url_params}&page=#{current_page + 1}"
     }
