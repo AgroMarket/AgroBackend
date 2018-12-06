16efb09f6f5e788585da103a223812a7f36bbe27
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-06 12:56:02 +0700

    refactor transactions table

--- db/migrate/20181201104735_create_transactions.rb
+++ db/migrate/20181201104735_create_transactions.rb
@@ -1,12 +1,14 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-      t.references :from, index: true
-      t.references :to
-      t.integer :amount
-      t.references :ask, null: true, default: 0
-      t.references :order, null: true, default: 0
-      t.integer :status
+      t.integer :type,      null: false, default: 0
+      t.integer :from,      null: false, default: 0
+      t.integer :to,        null: false, default: 0
+      t.integer :amount,    null: false, default: 0
+      t.integer :status,    null: false, default: 0
+      t.references :ask,    null: false, default: 0
+      t.references :order,  null: false, default: 0
+      t.references :task,   null: false, default: 0
 
       t.timestamps
     end
