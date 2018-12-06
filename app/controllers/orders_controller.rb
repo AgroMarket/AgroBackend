779cc2bc7fdaf1798dc28a44cba61d1dd07f4793
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-06 11:41:08 +0700

    clear orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user
-  before_action :set_order, only: [:show, :update, :destroy]
   include Exceptable
-
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
-  end
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
 
   # POST /orders
   # POST /orders.json
@@ -46,30 +34,10 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    if @order.update(order_params)
-      render :show, status: :ok, location: @order
-    else
-      render json: @order.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
-      params.require(:order).permit(:id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def order_params
+    params.require(:order).permit(:id)
+  end
 end
