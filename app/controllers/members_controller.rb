73084dfaf8f19f5a7e88984612687fc47f554fb8
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-06 11:42:09 +0700

    refactor members_controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,53 +1,31 @@
 class MembersController < ApplicationController
-  before_action :set_member, only: [:show, :update, :destroy]
+  before_action :set_member, only: :show
+  include Exceptable
 
-  # GET /members
-  # GET /members.json
-  def index
-    @members = Member.all
-  end
+  # GET /api/members/1
+  # GET /api/members/1.json
+  def show; end
 
-  # GET /members/1
-  # GET /members/1.json
-  def show
-  end
-
-  # POST /members
-  # POST /members.json
+  # POST /api/members
+  # POST /api/members.json
   def create
-    @member = Member.new(member_params)
-
-    if @member.save
-      render :show, status: :created, location: @member
-    else
-      render json: @member.errors, status: :unprocessable_entity
+    build do
+      @member   = Member.find_by(email: member_params[:email])
+      @member ||= Member.create!(member_params)
+      message    'Данные пользователя'
+      view       'members/show'
     end
-  end
-
-  # PATCH/PUT /members/1
-  # PATCH/PUT /members/1.json
-  def update
-    if @member.update(member_params)
-      render :show, status: :ok, location: @member
-    else
-      render json: @member.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /members/1
-  # DELETE /members/1.json
-  def destroy
-    @member.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_member
-      @member = Member.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def member_params
-      params.require(:member).permit(:amount, :type, :image, :name, :address, :phone, :description, :producer_logo, :producer_brand, :producer_address, :producer_phone, :producer_description, :producer_inn)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_member
+    @member = Member.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def member_params
+    params.require(:member).permit(:amount, :user_type, :email, :password, :name, :address, :phone, :description, :producer_brand, :producer_address, :producer_phone, :producer_description, :producer_inn)
+  end
 end
