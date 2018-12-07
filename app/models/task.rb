3a389ae9b11205652ff77f38df701601400c9648
Dmitry Fursenko <dfursenko@gmail.com>
2018-12-07 19:50:59 +0700

    refactor Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,12 @@ class Task < ApplicationRecord
   belongs_to :member
 
   enum status: %i[Ожидает Доставлен]
+
+  after_update :ask_delivered
+
+  def ask_delivered
+    return unless status == 'Доставлен'
+
+    ask.update status: 'Доставлен'
+  end
 end
