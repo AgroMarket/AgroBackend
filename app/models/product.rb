59cd0686382cec3fc7d728a5c95cc02b5221cb50
Alexey Tyugaev <alexx10@mail.ru>
2018-12-08 23:21:43 +0300

    model/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,10 +7,7 @@ class Product < ApplicationRecord
   has_many :order_items
   has_one_attached :image
 
-  scope :active, -> {where("deleted IS FALSE")}
-  scope :inactive, -> {where("deleted IS TRUE")}
-
-  # validates :name, presence: :true
+    # validates :name, presence: :true
 
   scope :by_category, ->(category_id) { where(category_id: category_id) }
   scope :by_parent_categories, ->(category_id) do
@@ -23,6 +20,8 @@ class Product < ApplicationRecord
   scope :by_producer, ->(producer_id) { where producer_id: producer_id }
   scope :search, ->(string) { where('LOWER(name) LIKE ?', "%#{string.downcase}%") }
   scope :samples, -> { find(pluck(:id).sample(8)) }
+  scope :active, -> {where("deleted IS FALSE")}
+  scope :inactive, -> {where("deleted IS TRUE")}
 
   def self.missing_png
     { io: File.open("#{Rails.root}/app/assets/images/300x300/missing.png"), filename: 'missing.png' }
